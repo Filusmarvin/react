cfb9b5a8165d3f92ca6224c1af27db34fdeb05d6
Marvin Filus <marvinfi@Marvins-MacBook-Air.local>
2017-04-24 13:21:01 -0400

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+// This places the origin aka entry point on the screen.
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
